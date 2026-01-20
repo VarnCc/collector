089b35e3ed2e0e96bacab30dadb1ad2918653a3a
VarnCc <kevin.s.96@hotmail.de>
2026-01-20 13:14:28 +0100

    item disappears by contact player and ground

--- items.gd
+++ items.gd
@@ -14,3 +14,5 @@ func _process(delta: float) -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		queue_free()
+	elif body.is_in_group("ground"):
+		queue_free()
--- player.gd
+++ player.gd
@@ -5,8 +5,7 @@ const SPEED = 300.0
 
 func _physics_process(delta: float) -> void:
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
