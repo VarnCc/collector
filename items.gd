fae8933a43ea3599b38cb00268ff29a546fa2143
VarnCc <kevin.s.96@hotmail.de>
2026-01-20 11:24:50 +0100

    add iteam logic

---
+++ items.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite2D.play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		queue_free()
